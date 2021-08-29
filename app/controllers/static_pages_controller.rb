8c9e565076e84657bc6b2976e8eadeb1e0aff698
Grimhamr <rossiadisj@gmail.com>
2021-08-29 17:40:01 +0300

    add figaro

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,15 @@
 class StaticPagesController < ApplicationController
 
     def index
-        
+        flickr = Flickr.new
+
+        #@photos
+
+        respond_to do |format|
+            format.html # index.html.erb
+            format.xml  { render :xml => @photos }
+            format.json { render :json => @photos }
+
+            end
     end
 end
