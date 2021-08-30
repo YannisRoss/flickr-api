96f0258ed7c35abdb2352c8fcbf6ce66f7e8987d
Grimhamr <rossiadisj@gmail.com>
2021-08-30 10:00:18 +0300

    show photos on index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,17 @@
 class StaticPagesController < ApplicationController
+require 'flickr'
 
     def index
       
-        Flickr.new(Figaro.env.flickr_api_key,Figaro.env.flickr_shared_secret)
+        flickr = Flickr.new
+#(Figaro.env.flickr_api_key,Figaro.env.flickr_shared_secret)
+
+        unless params[:user_id].blank?
+            @photos = flickr.photos.search(user_id: params[:user_id])
+          else
+            @photos = flickr.photos.getRecent
+          end
+
         respond_to do |format|
             format.html # index.html.erb
             format.xml  { render :xml => @photos }
@@ -11,4 +20,3 @@ class StaticPagesController < ApplicationController
             end
     end
 end
-ENV["FLICKR_SHARED_SECRET"]
